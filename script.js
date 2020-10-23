d398c7b5aa493356681aeb9fc41c4575b81f4df8
John Robinson <johnarobinson133@gmail.com>
2020-10-22 18:16:42 -0600

    added some functions to the js file

---
+++ script.js
@@ -0,0 +1,54 @@
+var cityEl = $("#city");
+var displayForcastEl = $("#display-forecast");
+var displayWeather = $("#display");
+var searchHistoryEl = $("#search-history");
+
+
+var getWeather = function(city) {
+    
+};
+
+
+
+var getForecast = function(city) {
+
+
+};
+
+
+var citySearch = function(city) {
+
+
+};
+
+
+var displayWeather = function(data) {
+
+
+};
+
+
+var displayForecast = function(data) {
+
+
+};
+
+
+var getIcon = function(iconID) {
+
+
+};
+
+
+var getUVIndex = function(latitude, longitude) {
+
+
+};
+
+
+var saveSearch = function(city) {
+
+
+};
+
+
